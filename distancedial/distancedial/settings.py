9045f532a2f411f9a71d0eb2cd854fb566e0a64f
Leslie Mathys <leslie@Leslies-MacBook-Air.local>
2024-10-24 21:04:15 +1100

    downloaded more packages

--- distancedial/distancedial/settings.py
+++ distancedial/distancedial/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_htmx',
+    'pwa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
