7072a4171e5070a5c9b5f46bbb2b7cd9b0283375
Leslie Mathys <leslie@Leslies-MacBook-Air.local>
2024-11-08 23:19:30 +1100

    written nav

--- distancedial/marketing_site/views.py
+++ distancedial/marketing_site/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
-    template_name = "marketing_site/index.html"
+    template_name = "pages/base.html"
+
+def home(request):
+    return render(request, 'pages/base.html')
