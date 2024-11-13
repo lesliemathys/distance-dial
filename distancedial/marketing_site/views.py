9886986cc1be5b02f939489240ab17faf22f2041
Leslie Mathys <leslie@Leslies-MacBook-Air.local>
2024-11-13 21:40:58 +1100

    added more to index.html

--- distancedial/marketing_site/views.py
+++ distancedial/marketing_site/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
-    template_name = "pages/base.html"
+    template_name = "pages/index.html"
 
 def home(request):
-    return render(request, 'pages/base.html')
+    return render(request, 'pages/index.html')
